fa9f75dd3fb3880ddea29140c089dd4aaeeca476
MathildeMerx <mathilde.merx0@gmail.com>
2022-07-19 11:20:00 +0200

    Fix the order of points in the dropdown when creating a segment

--- src/designer/SegmentLogic/SegmentSelectPoints.jsx
+++ src/designer/SegmentLogic/SegmentSelectPoints.jsx
@@ -115,7 +115,15 @@ function DropdownMenu({ points, newSegment, setNewSegment, index }) {
 
             <S_DropdownContent clicked={clicked}>
                 {Object.keys(points)
-                    .sort()
+                    .sort((a, b) => {
+                        if (a.length === b.length) {
+                            if (a[0] === b[0] || a.length === 1) {
+                                return a < b ? -1 : 1;
+                            }
+                            return a[1] < b[1] ? -1 : 1;
+                        }
+                        return a.length < b.length ? -1 : 1;
+                    })
                     .map((pointName) => {
                         return (
                             <DropdownItem
