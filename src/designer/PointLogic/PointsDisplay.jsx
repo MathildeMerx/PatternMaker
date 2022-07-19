17a38f10149a821e3cb377b16ff590b7d9599d49
MathildeMerx <mathilde.merx0@gmail.com>
2022-07-19 11:21:37 +0200

    Fix the order of points in the point display

--- src/designer/PointLogic/PointsDisplay.jsx
+++ src/designer/PointLogic/PointsDisplay.jsx
@@ -19,7 +19,15 @@ function PointsDisplay({ points, height }) {
             {Object.keys(points).length !== 0 ? (
                 <S_ControlledHeightUL height={height}>
                     {Object.keys(points)
-                        .sort()
+                        .sort((a, b) => {
+                            if (a.length === b.length) {
+                                if (a[0] === b[0] || a.length === 1) {
+                                    return a < b ? -1 : 1;
+                                }
+                                return a[1] < b[1] ? -1 : 1;
+                            }
+                            return a.length < b.length ? -1 : 1;
+                        })
                         .map((point) => (
                             <S_li key={point}>
                                 {`${point} (${points[point][0]}, ${points[point][1]})`}
